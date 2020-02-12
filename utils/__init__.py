12617aa08e190deba7e6ecedc97a4924ae53bbb7
a <eliasnwr@gmail.com>
2020-02-12 20:57:01 +0300

    added missing import

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 from .common_utils import get_teams_list
 from .common_utils import read_break_time, read_circles_number
 from .common_utils import read_group_name, read_group_size
+from .protocol_utils import generate_protocol_doc
 from .schedule_utils import generate_schedule_doc
-__all__ = []
